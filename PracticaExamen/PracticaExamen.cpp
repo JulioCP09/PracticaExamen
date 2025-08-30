fb012b9e10fe850c381b6a52f67220af0573911c
JULIO CUBILLO PE�ARANDA <julio.cubillo.penaranda@est.una.ac.cr>
2025-08-29 18:51:20 -0600

    Agrego class Vente

--- PracticaExamen/PracticaExamen.cpp
+++ PracticaExamen/PracticaExamen.cpp
@@ -3,7 +3,54 @@
 
 #include <iostream>
 
+using namespace std;
+
+class Venta
+{
+private:
+	int precioProducto;
+public:
+	Venta(int producto) : precioProducto(producto) {}
+
+	void compra() 
+	{
+		int cantidadCompra = 0;
+		float totalCompra = 0;
+		cout << "***********************************" << endl;
+		cout << "Producto: Consola de Videojuegos" << endl;
+		cout << "Precio del Producto: $350" << endl;
+		cout << endl;
+		cout << "Cuantos aticulos desea comprar: " << endl;
+		cin >> cantidadCompra;
+
+		if (cantidadCompra > 5) 
+		{
+			if (cantidadCompra > 7) 
+			{
+				totalCompra = cantidadCompra * precioProducto;
+				cout << "Total de la compra: " << totalCompra << endl;
+				cout << "Descuento a aplicar del 15%: "<< totalCompra * 0.15 << endl;
+				cout << "total de la compra con el descuento aplicado: "<< totalCompra - (totalCompra * 0.15) << endl;
+			}
+			else 
+			{
+				totalCompra = cantidadCompra * precioProducto;
+				cout << "Total de la compra: " << totalCompra << endl;
+				cout << "Descuento a aplicar del 10%: " << totalCompra * 0.1 << endl;
+				cout << "total de la compra con el descuento aplicado: " << totalCompra - (totalCompra * 0.1) << endl;
+			}
+		}
+		else 
+		{
+			cout << "Descuento a aplicar del 0%: " << endl;
+			cout << "Total de la compra: " << cantidadCompra * precioProducto << endl;
+		}
+
+	}
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	Venta Consola(350);
+	Consola.compra();
 }
